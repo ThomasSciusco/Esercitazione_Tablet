c11e38a4d8e021c16672a9bc1b224c7995c6b27b
Sciusco.20197 <Sciusco.20197@E2-9-PC06>
2023-10-12 10:49:28 +0200

    Aggiungere i file di progetto.

---
+++ Esercitazione_Tablet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Eserciazione_tablet
+{
+
+    public class tablet
+    {
+        public string marca;
+        public float dimensioni;
+        public float batteria;
+        public float velocità;
+
+        public tablet(string marca,float dimensioni,float batteria,float velocità) 
+        {
+            this.marca = marca;
+            this.dimensioni = dimensioni;
+            this.batteria = batteria;
+            this.velocità = velocità;
+        }
+
+    }
+}
